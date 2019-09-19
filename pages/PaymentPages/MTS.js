4277ae716aa0f5849ed3e22d32ac519168340d68
Artem Abakumov <hackintosh@iMac-Artem.local>
2019-09-19 12:28:40 +0400

    add MTS page

--- pages/PaymentPages/MTS.js
+++ pages/PaymentPages/MTS.js
@@ -1,10 +1,17 @@
 import Layout from '../index'
+import Header from '../../src/components/Header/Header'
+import styled from 'styled-components'
 
+const MtsPage = styled.div`
+`
 
 export default function MTS(props) {
  
     return (
-            <p>This is the about page</p>
+      <MtsPage>
+      <Header />
+        <p>This is the about page</p>
+      </MtsPage>
     );
   
   }
